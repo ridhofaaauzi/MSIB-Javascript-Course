ff4cd5469b6b3e9b336c5b82859e572c1f3cd77e
Ridho Fauzi Grafika <ridh21142ti@student.nurulfikri.ac.id>
2024-03-07 21:39:18 +0700

    Tugas JS 1

---
+++ Tugas-1-Soal/switch.js
@@ -0,0 +1,30 @@
+/**
+ * Tolong perhatikan secara SEKSAMA Langkah berikut ini
+ * 1. Buatlah sebuah switch case dengan ketentuan sebagai berikut :
+ * - Jika value hewan merupakan kucing maka akan menampilkan "meow" di termimal
+ * - Jika value hewan merupakan sapi maka akan menampilkan "moo" di termimal
+ * - Jika value hewan merupakan kambing maka akan menampilkan "mbee" di terminal.
+ * - Jika value hewan merupakan ayam maka maka akan menampilkan "kukuruyuk" di terminal
+ * - Jika tidak ada yang sama, maka akan menampilkan "Hewan tidak diketahui"
+ * PERHATIAN : Pembuatan kondisi diatas HARUS menggunakan switch case
+ */
+
+var hewan = "kucing"; // sapi, kambing, kucing, ayam null
+
+switch (hewan) {
+  case "kucing":
+    console.log("meow");
+    break;
+  case "sapi":
+    console.log("moo");
+    break;
+  case "kambing":
+    console.log("mbee");
+    break;
+  case "ayam":
+    console.log("kukuruyuk");
+    break;
+  default:
+    console.log("Hewan tidak diketahui");
+    break;
+}
---
+++ Tugas-1-Soal/variable.js
@@ -0,0 +1,9 @@
+/**
+ * 1. Buatlah Sebuah variabel bernama hello
+ * 2. Isi variabel tersbeut dengan nilai string:
+ *  Halo Dunia
+ * 3. Tampilkan isi nilai variabel hello tersebut pada terminal
+ */
+
+var hello = "Halo Dunia";
+console.log(hello);
